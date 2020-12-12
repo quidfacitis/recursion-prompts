b7a8793df996abf6810db85810d1524adc1dd85e
Ross Loney <majoross@gmail.com>
2020-12-12 14:07:19 -0600

    sumBelow (5)

--- src/recursion.js
+++ src/recursion.js
@@ -67,6 +67,13 @@ var isEven = function(n) {
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n < 0) {
+    return ((n += sumBelow(n + 1)) + 1);
+  } else {
+    return ((n += sumBelow(n - 1)) - 1);
+  }
 };
 
 // 6. Get the integers within a range (x, y).
