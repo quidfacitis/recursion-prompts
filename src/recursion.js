0088879714c169d3e26e0f5c106bc418c05e49bb
Ross Loney <majoross@gmail.com>
2020-12-12 16:24:44 -0600

    power of two (8)

--- src/recursion.js
+++ src/recursion.js
@@ -102,6 +102,14 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  if (exp === 0) {
+    return 1;
+  }
+  if (exp > 0) {
+    return (base * exponent(base, (exp - 1)));
+  } else {
+    return ((1/base) * exponent(base, (exp + 1)));
+  }
 };
 
 // 8. Determine if a number is a power of two.
@@ -109,6 +117,12 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+  if (n === 1 || n === 2) {
+    return true;
+  } else if (n < 1) {
+    return false;
+  }
+  return powerOfTwo(n / 2);
 };
 
 // 9. Write a function that reverses a string.
