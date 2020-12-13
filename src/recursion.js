2164c38d327a6809f4c4d92c465492df287efd6f
Ross Loney <majoross@gmail.com>
2020-12-12 19:23:33 -0600

    multiply (12)

--- src/recursion.js
+++ src/recursion.js
@@ -165,11 +165,38 @@ var palindrome = function(string) {
 // modulo(17,5) // 2
 // modulo(22,6) // 4
 var modulo = function(x, y) {
+  if (x === 0 && y === 0) {
+    return NaN;
+  }
+
+  // figure out how to accept negative numbers
+
+  if (x === 0) {
+    return 0;
+  } else if (x < 0) { 
+    return x + y;
+  }
+  
+  return modulo((x - y), y);
 };
 
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
+  
+  if (y === 0) {
+    return 0;
+  }
+  
+  if (y < 0 && x > 0) {
+    return -x + multiply(x, y + 1);
+  } else if (y > 0 && x < 0) {
+    return x + multiply(x, y - 1);
+  } else if (y < 0 && x < 0) {
+    return -x + multiply(x, y + 1);
+  } else { 
+    return x + multiply(x, y - 1)
+  }
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
